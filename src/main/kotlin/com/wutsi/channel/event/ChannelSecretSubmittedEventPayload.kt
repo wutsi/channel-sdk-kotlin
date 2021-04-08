8116325f8e1773bbe5b9bdb5238e2c000cc9beaa
htchepannou <htchepannou@expedia.com>
2021-04-08 15:19:28 -0400

    rename twitterId -> channelUserId

--- src/main/kotlin/com/wutsi/channel/event/ChannelSecretSubmittedEventPayload.kt
+++ src/main/kotlin/com/wutsi/channel/event/ChannelSecretSubmittedEventPayload.kt
@@ -3,7 +3,7 @@ package com.wutsi.channel.event
 data class ChannelSecretSubmittedEventPayload(
   val userId: Long = -1,
   val siteId: Long = 0,
-  val twitterId: Long = 0,
+  val channelUserId: String = "",
   val accessToken: String = "",
   val accessTokenSecret: String = "",
   val type: String = ""
